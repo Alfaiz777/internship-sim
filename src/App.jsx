9f77f49f0d26fc58a7a3804a24b4385f08f2b9a6
Alfaiz777 <alfaizkureshi111@gmail.com>
2025-08-24 15:24:15 +0530

    feat: setup react project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-amber-200 text-center">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
